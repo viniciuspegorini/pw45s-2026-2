43fb98188e07ecc313bb3b4fc4a106f992e4e0bd
Vinicius Pegorini <viniciuspegorini@gmail.com>
2026-08-31 18:51:21 -0300

    aula02-2026-08-31

---
+++ aula02/server/src/main/java/br/edu/utfpr/pb/pw45s/server/controller/UserController.java
@@ -0,0 +1,31 @@
+package br.edu.utfpr.pb.pw45s.server.controller;
+
+import br.edu.utfpr.pb.pw45s.server.dto.UserDTO;
+import br.edu.utfpr.pb.pw45s.server.mapper.UserMapper;
+import br.edu.utfpr.pb.pw45s.server.service.UserService;
+import br.edu.utfpr.pb.pw45s.server.shared.GenericResponse;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("users")
+public class UserController {
+
+    private final UserService userService;
+    private final UserMapper userMapper;
+
+    public UserController(UserService userService, UserMapper userMapper) {
+        this.userService = userService;
+        this.userMapper = userMapper;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<GenericResponse> createUser(@RequestBody @Valid UserDTO userDTO) {
+        this.userService.save(userMapper.toEntity(userDTO));
+        return ResponseEntity.status(HttpStatus.CREATED).body(new GenericResponse("Usuário salvo com sucesso"));
+    }
+
+}
---
+++ aula02/server/src/main/java/br/edu/utfpr/pb/pw45s/server/dto/ProductDTO.java
@@ -0,0 +1,26 @@
+package br.edu.utfpr.pb.pw45s.server.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProductDTO {
+
+    private Long id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private String description;
+
+    @NotNull
+    private BigDecimal price;
+
+    private CategoryDTO category;
+}
